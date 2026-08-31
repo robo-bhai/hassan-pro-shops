4c8d64d1cd601738e355c39836e8d44b1d4533b2
mnomanbh-source <m.noman.bh.com>
2026-08-31 06:55:19 +0500

    +++

--- P1/settings.py
+++ P1/settings.py
@@ -179,7 +179,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
