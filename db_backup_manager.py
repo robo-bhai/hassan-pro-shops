bd55bd8ddb9e8c91109e025b88bf928324011333
mnomanbh-source <m.noman.bh.com>
2026-09-07 18:35:42 +0500

    ..

--- db_backup_manager.py
+++ db_backup_manager.py
@@ -89,10 +89,11 @@ def export_backup(output_filename=None):
             backup_data[table] = rows
 
     json_payload = json.dumps(
-        backup_data, 
-        default=lambda o: o.isoformat() if isinstance(o, (datetime.date, datetime.datetime)) else str(o)
+        backup_data,
+        default=lambda o: o.isoformat() if hasattr(o, "isoformat") else str(o),
     )
     encrypted_content = encrypt_data(json_payload)
+
 
 
     with open(output_filename, "wb") as f:
