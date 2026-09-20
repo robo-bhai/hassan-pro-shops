186d475bc8ba51947a6b46950f680855bda45dad
robo-bhai <m.noman.bh.com>
2026-09-20 13:44:46 +0500

    Auto update

--- P1/urls.py
+++ P1/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.shortcuts import redirect
-
+from django.urls import path, include, re_path 
 # Handlers bind karein app.views ke sath
 handler404 = 'app.views.custom_page_not_found'
 handler500 = 'app.views.custom_server_error'
